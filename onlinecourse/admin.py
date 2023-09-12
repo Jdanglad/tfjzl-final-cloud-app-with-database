aa24ae32d376e1b44ea7665e2342b9744dd84dde
Jdanglad <dangladjose@gmail.com>
2023-09-12 03:37:32 -0400

    Last implementations to django final project

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -5,34 +5,35 @@ class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-"""Choices class"""
+#In line choice model
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
 
-"""Questions class"""
+#In line question model
 class QuestionInline(admin.StackedInline):
-    models = Question
+    model = Question
     extra = 2
 
+# Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
-"""Question admin, adding question to lessons"""
+# Question Admin model
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['content']
 
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
-admin.site.register(Instructor)
-admin.site.register(Learner)
